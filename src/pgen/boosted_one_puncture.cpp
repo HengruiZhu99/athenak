fe4a3faab4c2677a6becd1d7ffb23cf673d43ee0
Hengrui Zhu <hz0693@princeton.edu>
2024-09-26 09:01:38 -0400

    local changes

--- src/pgen/boosted_one_puncture.cpp
+++ src/pgen/boosted_one_puncture.cpp
@@ -100,22 +100,22 @@ void ADMOnePunctureBoosted(MeshBlockPack *pmbp, ParameterInput *pin) {
     Real &x1min = size.d_view(m).x1min;
     Real &x1max = size.d_view(m).x1max;
     int nx1 = indcs.nx1;
-    Real x1v = CellCenterX(i-is, nx1, x1min, x1max);
+    Real x = CellCenterX(i-is, nx1, x1min, x1max);
 
     Real &x2min = size.d_view(m).x2min;
     Real &x2max = size.d_view(m).x2max;
     int nx2 = indcs.nx2;
-    Real x2v = CellCenterX(j-js, nx2, x2min, x2max);
+    Real y = CellCenterX(j-js, nx2, x2min, x2max);
 
     Real &x3min = size.d_view(m).x3min;
     Real &x3max = size.d_view(m).x3max;
     int nx3 = indcs.nx3;
-    Real x3v = CellCenterX(k-ks, nx3, x3min, x3max);
+    Real z = CellCenterX(k-ks, nx3, x3min, x3max);
 
     // Shift coordinates to black hole center
-    x1v -= center_x1;
-    x2v -= center_x2;
-    x3v -= center_x3;
+    x -= center_x1;
+    y -= center_x2;
+    z -= center_x3;
 
     // Velocity magnitude
     Real vel = std::sqrt(vx1*vx1 + vx2*vx2 + vx3*vx3);
@@ -124,16 +124,14 @@ void ADMOnePunctureBoosted(MeshBlockPack *pmbp, ParameterInput *pin) {
     Real Gamma = 1.0 / std::sqrt(1.0 - vel * vel);
 
     // Coordinates in comoving frame (x0)
-    Real x0[4]; // x0[0] = t0, x0[1] = x0, x0[2] = y0, x0[3] = z0
-
     // Lorentz transformation along x-direction
-    x0[1] = Gamma * (x1v - vel * 0.0); // At t = 0
-    x0[0] = -vel * x0[1]; // t0 = -v * x0
-    x0[2] = x2v;
-    x0[3] = x3v;
+    // TODO (@hzhu): Add rotation here.
+    Real x0 = Gamma * x; // At t = 0
+    Real y0 = y;
+    Real z0 = z;
 
     // Radial coordinate in comoving frame
-    Real r0 = std::sqrt(x0[1]*x0[1] + x0[2]*x0[2] + x0[3]*x0[3]);
+    Real r0 = std::sqrt(x0*x0 + y0*y0 + z0*z0);
 
     // Compute psi0 and its derivative
     Real psi0 = 1.0 + m0 / (2.0 * r0);
@@ -156,57 +154,15 @@ void ADMOnePunctureBoosted(MeshBlockPack *pmbp, ParameterInput *pin) {
     Real B0 = std::sqrt(B0_squared);
 
     // Compute the lapse function alpha
-    Real alpha = alpha0 / B0;
+    adm.alpha(m,k,j,i) = alpha0 / B0;
 
     // Compute the shift vector beta^i (only beta^x is non-zero)
     Real num_beta = alpha0_2 - psi0_4;
     Real den_beta = psi0_4 - alpha0_2 * vel * vel;
-    Real beta_x = (num_beta / den_beta) * vel;
-    Real beta_y = 0.0;
-    Real beta_z = 0.0;
-
-    // Compute s and its derivative
-    Real s = den_beta; // s = psi0^4 - alpha0^2 * v^2
-    Real psi0_prime_4 = 4.0 * psi0 * psi0 * psi0 * psi0_prime;
-    Real alpha0_prime_2 = 2.0 * alpha0 * alpha0_prime;
-    Real s_prime = psi0_prime_4 - alpha0_prime_2 * vel * vel;
-    Real ln_s_prime = s_prime / s;
-
-    // Compute extrinsic curvature components
-    Real x = x1v;
-    Real y = x2v;
-    Real z = x3v;
-    Real r = r0;
-
-    // K_xx
-    Real prefactor_xx = Gamma * Gamma * B0 * x * vel / r;
-    Real expr_xx = 2.0 * alpha0_prime - (alpha0 / 2.0) * ln_s_prime;
-    Real K_xx = prefactor_xx * expr_xx;
-
-    // K_yy and K_zz
-    Real num_yy = 2.0 * Gamma * Gamma * x * vel * alpha0 * psi0_prime;
-    Real den_yy = psi0 * B0 * r;
-    Real K_yy = num_yy / den_yy;
-    Real K_zz = K_yy;
-
-    // K_xy and K_xz
-    Real prefactor_xy = B0 * vel / r;
-    Real expr_xy = alpha0_prime - (alpha0 / 2.0) * ln_s_prime;
-    Real K_xy = prefactor_xy * y * expr_xy;
-    Real K_xz = prefactor_xy * z * expr_xy;
-
-    // K_yz is zero due to symmetry
-    Real K_yz = 0.0;
-
-    // Set the ADM variables in the data structures
-
-    // Lapse function
-    adm.alpha(m,k,j,i) = alpha;
-
     // Shift vector components
-    adm.beta_u(m,0,k,j,i) = beta_x;
-    adm.beta_u(m,1,k,j,i) = beta_y;
-    adm.beta_u(m,2,k,j,i) = beta_z;
+    adm.beta_u(m,0,k,j,i) = (num_beta / den_beta) * vel;
+    adm.beta_u(m,1,k,j,i) = 0;
+    adm.beta_u(m,2,k,j,i) = 0;
 
     // Spatial metric components gamma_{ij}
     adm.g_dd(m,0,0,k,j,i) = psi0_4 * B0_squared;
@@ -216,13 +172,33 @@ void ADMOnePunctureBoosted(MeshBlockPack *pmbp, ParameterInput *pin) {
     adm.g_dd(m,0,2,k,j,i) = 0.0;
     adm.g_dd(m,1,2,k,j,i) = 0.0;
 
-    // Extrinsic curvature components K_{ij}
-    adm.vK_dd(m,0,0,k,j,i) = K_xx;
-    adm.vK_dd(m,1,1,k,j,i) = K_yy;
-    adm.vK_dd(m,2,2,k,j,i) = K_zz;
-    adm.vK_dd(m,0,1,k,j,i) = K_xy;
-    adm.vK_dd(m,0,2,k,j,i) = K_xz;
-    adm.vK_dd(m,1,2,k,j,i) = K_yz;
+    // Compute extrinsic curvature components
+    // Compute s and its derivative
+    Real s = den_beta; // s = psi0^4 - alpha0^2 * v^2
+    Real psi0_prime_4 = 4.0 * psi0 * psi0 * psi0 * psi0_prime;
+    Real alpha0_prime_2 = 2.0 * alpha0 * alpha0_prime;
+    Real s_prime = psi0_prime_4 - alpha0_prime_2 * vel * vel;
+    Real ln_s_prime = s_prime / s;
+
+    // K_xx
+    Real prefactor_xx = Gamma * Gamma * B0 * x * vel / r0;
+    Real expr_xx = 2.0 * alpha0_prime - (alpha0 / 2.0) * ln_s_prime;
+    adm.vK_dd(m,0,0,k,j,i) = prefactor_xx * expr_xx;
+
+    // K_yy and K_zz
+    Real num_yy = 2.0 * Gamma * Gamma * x * vel * alpha0 * psi0_prime;
+    Real den_yy = psi0 * B0 * r0;
+    adm.vK_dd(m,1,1,k,j,i)  = num_yy / den_yy;
+    adm.vK_dd(m,2,2,k,j,i)  = adm.vK_dd(m,1,1,k,j,i);
+
+    // K_xy and K_xz
+    Real prefactor_xy = B0 * vel / r0;
+    Real expr_xy = alpha0_prime - (alpha0 / 2.0) * ln_s_prime;
+    adm.vK_dd(m,0,1,k,j,i) = prefactor_xy * y * expr_xy;
+    adm.vK_dd(m,0,2,k,j,i) = prefactor_xy * z * expr_xy;
+
+    // K_yz is zero due to symmetry
+    adm.vK_dd(m,1,2,k,j,i) = 0.0;
   });
 }
 
